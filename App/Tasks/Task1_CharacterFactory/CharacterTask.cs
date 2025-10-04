cf5844593b7b4fcf50cbf6f303643f06e150da60
user1 <user1@DESKTOP-13ENB0J>
2025-10-04 14:37:34 +0300

    task 1

--- App/Tasks/Task1_CharacterFactory/CharacterTask.cs
+++ App/Tasks/Task1_CharacterFactory/CharacterTask.cs
@@ -1,1 +1,43 @@
 namespace App.Tasks.Task1_CharacterFactory;
+
+
+
+public interface ICharacter
+{
+    public string Name { get; set; }
+
+    public void Attack();
+
+}
+
+
+
+public class Warrior : ICharacter
+{
+    public string Name { get; set; }
+    public void Attack()
+    {
+        Console.WriteLine("Воин наносит удар мечом!");
+    }
+}
+
+public class Mage : ICharacter
+{
+    public string Name { get; set; }
+    public void Attack()
+    {
+        Console.WriteLine("Маг кастует огненный шар!");
+    }
+}
+
+
+public class CharacterFactory<T> where T : ICharacter, new()
+{
+    public T Create(string name)
+    {
+        var charecter = new T();
+        charecter.Name = name;
+        return charecter;
+
+    }
+}
