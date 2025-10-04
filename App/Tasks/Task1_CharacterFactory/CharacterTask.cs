ed106d21a3160ac74042510577cf2ac15eb25d5e
Danya <dansteamer1337@gmail.com>
2025-10-04 15:49:21 +0300

    Kuznetsov

--- App/Tasks/Task1_CharacterFactory/CharacterTask.cs
+++ App/Tasks/Task1_CharacterFactory/CharacterTask.cs
@@ -1,1 +1,37 @@
 namespace App.Tasks.Task1_CharacterFactory;
+
+public interface ICharacter
+{
+    string Name { get; set; }
+    public void Attack();
+}
+
+public class Warrior : ICharacter
+{
+    public string Name { get; set; }
+    public void Attack()
+    {
+        Console.WriteLine("Воин наносит удар мечом!");
+    }
+}
+public class Mage : ICharacter
+{
+    public string Name { get; set; }
+    public void Attack()
+    {
+        Console.WriteLine("Маг кастует огненный шар!");
+    }
+}
+
+public class CharacterFactory<T>
+    where T : ICharacter, new()
+{
+    public T Create(string name)
+    {
+        return new T()
+        {
+            Name = name
+        };
+        
+    }
+}
--- App/Tasks/Task3_Extensions/ExtensionsTask.cs
+++ App/Tasks/Task3_Extensions/ExtensionsTask.cs
@@ -1,1 +1,54 @@
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
 namespace App.Tasks.Task3_Extensions;
+
+
+public interface IDataRow
+{
+    public string GetFormattedString();
+}
+public class Employee : IDataRow
+{
+    public int Id { get; set; }
+    public string FullName { get; set; }
+    public string Position { get; set; }
+    public string GetFormattedString()
+    {
+        return $"ID: {Id}, Имя: {FullName}, Должность: {Position}";
+    }
+}
+public class Product : IDataRow
+{
+    public string Sku { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+
+    public string GetFormattedString()
+    {
+        var currentCulture = Thread.CurrentThread.CurrentCulture;
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        return $"Артикул: {Sku}, Название: {Name}, Цена: {Price}";
+        Thread.CurrentThread.CurrentCulture = currentCulture;
+    }
+}
+public static class CollectionExtensions
+{
+    public static void PrintToConsole<T>
+        (this IEnumerable<T> collection) 
+        where T : IDataRow
+    {
+        if (collection == null)
+        {
+            throw new ArgumentNullException();
+        }
+        else 
+        {
+            foreach (T item in collection)
+            {
+                Console.WriteLine(item.GetFormattedString());
+            }
+        }
+
+    }
+}
