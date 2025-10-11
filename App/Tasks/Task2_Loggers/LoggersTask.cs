507878fc1286abcf8b07ddfac3175b2736081693
user1 <user1@DESKTOP-ENRJJP4>
2025-10-11 14:15:46 +0300

    task 2

--- App/Tasks/Task2_Loggers/LoggersTask.cs
+++ App/Tasks/Task2_Loggers/LoggersTask.cs
@@ -1,1 +1,62 @@
-namespace App.Tasks.Task2_Loggers;
+using App.Tasks.Task2_Loggers;
+using System;
+
+namespace App.Tasks.Task2_Loggers
+{
+    public class ObjectLogger<T> where T : class
+    {
+        public void Log(T data)
+        {
+            if (data == null)
+            {
+                Console.WriteLine("Объект пуст (null)");
+            }
+            else
+            {
+                Console.WriteLine(data.ToString());
+            }
+        }
+    }
+
+    public class ValueLogger<T> where T : struct
+    {
+        public void Log(T data)
+        {
+            Console.WriteLine(data.ToString());
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== Демонстрация ObjectLogger ===");
+
+        var stringLogger = new ObjectLogger<string>();
+        stringLogger.Log("hello");
+        stringLogger.Log(null);
+
+        var objectLogger = new ObjectLogger<object>();
+        objectLogger.Log(new { Name = "Test", Value = 123 });
+        objectLogger.Log(null);
+
+        Console.WriteLine("\n=== Демонстрация ValueLogger ===");
+
+        var dateTimeLogger = new ValueLogger<DateTime>();
+        dateTimeLogger.Log(DateTime.Now);
+
+        var intLogger = new ValueLogger<int>();
+        intLogger.Log(42);
+
+        var doubleLogger = new ValueLogger<double>();
+        doubleLogger.Log(3.14159);
+
+        var boolLogger = new ValueLogger<bool>();
+        boolLogger.Log(true);
+
+        Console.WriteLine("\n=== Попытка создания неправильных логгеров ===");
+
+        Console.WriteLine("Программа завершена успешно!");
+    }
+}
